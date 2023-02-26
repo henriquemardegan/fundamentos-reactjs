f6508ec938836e80c8d95df6b35c0e6c06295a91
Luiz Henrique Mardegan <luizhenriquemardegan@gmail.com>
2023-02-26 02:31:16 -0400

    Primeiro commit

---
+++ src/components/Comment.jsx
@@ -0,0 +1,47 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+export function Comment(props) {
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment() {
+        props.onDeleteComment(props.content)
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1;
+        });
+    }
+
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/henriquemardegan.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Luiz Henrique</strong>
+                            <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30">Publicado há 1h</time>
+                        </div>
+
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>{props.content}</p>
+                </div>
+
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>  
+                </footer>
+            </div>
+        </div>
+    )
+}
